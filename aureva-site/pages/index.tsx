2bdd0f6335c7cdc340978ba53ec4be50256aebf1
Aureva <andre@aytyrealty.com.br>
2026-06-22 22:21:38 -0300

    Update index.tsx

--- aureva-site/pages/index.tsx
+++ aureva-site/pages/index.tsx
@@ -138,7 +138,7 @@ export default function Home() {
         <meta name="description" content="Aureva é um marketplace premium de lifestyle para a melhor idade." />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
